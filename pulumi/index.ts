3669fa698dc9ef050444964fe7bceb19f4f78332
Tayven Bigelow <gwathruoko@gmail.com>
2026-03-07 16:23:36 -0700

    feat: initial infrastructure and configuration setup

---
+++ pulumi/index.ts
@@ -0,0 +1,7 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as proxmox from "@muhlba91/pulumi-proxmoxve";
+
+// Example base configuration
+// Real configurations for TrueNAS, Talos, and Ubuntu Plex will be defined here.
+
+export const message = "Pulumi setup initialized.";
