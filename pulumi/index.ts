28babf7c414c952e0df1ba9cfe4249a968700929
Tayven Bigelow <gwathruoko@gmail.com>
2026-03-07 16:34:23 -0700

    feat: migrate pulumi to go with project proxmox and stack chalupa-local

--- pulumi/index.ts
+++
@@ -1,7 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as proxmox from "@muhlba91/pulumi-proxmoxve";
-
-// Example base configuration
-// Real configurations for TrueNAS, Talos, and Ubuntu Plex will be defined here.
-
-export const message = "Pulumi setup initialized.";
